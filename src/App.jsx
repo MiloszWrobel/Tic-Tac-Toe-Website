b2b277abd1b27475169dbdb3b79b1eee34b2d84b
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2023-12-08 13:30:31 +0000

    Created player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Player from "./components/Player";
 function App() {
-  return <p>Working</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X"></Player>
+          <Player name="Player 2" symbol="O"></Player>
+        </ol>
+        game boards
+      </div>
+      Log
+    </main>
+  );
 }
 
 export default App;
