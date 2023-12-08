8f9144a1cff528085d57d5c6d95877bbe3976d13
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2023-12-08 13:44:06 +0000

    Added interactivity to edit button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleClick() {
+    setIsEditing(true);
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? <input /> : <span className="player-name">{name}</span>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button> Edit</button>
+      <button onClick={handleClick}> Edit</button>
     </li>
   );
 }
