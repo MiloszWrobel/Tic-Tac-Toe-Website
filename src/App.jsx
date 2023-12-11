0aa8515001fbc18414ef3e6513a54d4e0b937d3d
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2023-12-11 12:19:07 +0000

    Added game board functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 function App() {
   return (
     <main>
@@ -7,7 +8,7 @@ function App() {
           <Player initialName="Player 1" symbol="X"></Player>
           <Player initialName="Player 2" symbol="O"></Player>
         </ol>
-        game boards
+        <GameBoard />
       </div>
       Log
     </main>
